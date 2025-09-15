723ea991e4efda2e64ea191a3107b6b4bd281e58
carol-leal <carolinalealrequena@gmail.com>
2025-09-15 19:21:55 +0200

    Missing Discord

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,11 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle({
-  client: sql,
-  schema,
-  casing: "snake_case",
-});
+if (!process.env.POSTGRES_URL) {
+  throw new Error("Missing POSTGRES_URL");
+}
+const client = postgres(process.env.POSTGRES_URL);
+
+export const db = drizzle(client, { schema, casing: "snake_case" });

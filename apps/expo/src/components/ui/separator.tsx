f6d876dba1b5515ea04741f67a1af0d1f77057d0
Carol <carolinalealrequena@gmail.com>
2025-09-20 20:49:58 +0200

    Lint fixed

--- apps/expo/src/components/ui/separator.tsx
+++ apps/expo/src/components/ui/separator.tsx
@@ -1,20 +1,22 @@
-import { cn } from '~/lib/utils';
-import * as SeparatorPrimitive from '@rn-primitives/separator';
+import * as SeparatorPrimitive from "@rn-primitives/separator";
+
+import { cn } from "~/lib/utils";
 
 function Separator({
   className,
-  orientation = 'horizontal',
+  orientation = "horizontal",
   decorative = true,
   ...props
-}: SeparatorPrimitive.RootProps & React.RefAttributes<SeparatorPrimitive.RootRef>) {
+}: SeparatorPrimitive.RootProps &
+  React.RefAttributes<SeparatorPrimitive.RootRef>) {
   return (
     <SeparatorPrimitive.Root
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        'bg-border shrink-0',
-        orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
-        className
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className,
       )}
       {...props}
     />
